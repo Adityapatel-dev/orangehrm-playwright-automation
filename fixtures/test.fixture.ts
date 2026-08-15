d83fc4a8e5330145dd4186c8b5f7b1adc1458bbd
Adityapatel-dev <adityapatel1998oct@gmail.com>
2026-08-15 12:24:24 +0530

    refactor: simplify authentication architecture

--- fixtures/test.fixture.ts
+++ fixtures/test.fixture.ts
@@ -1,14 +1,11 @@
-import { test as base, expect, Page } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 
 import { LoginPage } from '../pages/LoginPage';
 import { DashboardPage } from '../pages/DashboardPage';
-import { loginData } from '../data/LoginData';
-
 
 type TestFixtures = {
   loginPage: LoginPage;
   dashboardPage: DashboardPage;
-  authenticatedPage: Page;
 };
 
 export const test = base.extend<TestFixtures>({
@@ -22,23 +19,6 @@ export const test = base.extend<TestFixtures>({
     const dashboardPage = new DashboardPage(page);
 
     await use(dashboardPage);
-  },
-
-  authenticatedPage: async ({ page }, use) => {
-    const loginPage = new LoginPage(page);
-
-    await page.goto('/web/index.php/auth/login', {
-      waitUntil: 'commit',
-    });
-
-    await loginPage.login(
-      loginData.validUser.username,
-      loginData.validUser.password
-    );
-
-    await page.waitForURL(/dashboard/);
-
-    await use(page);
   },
 });
 
